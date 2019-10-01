ab7eb4d5ee6a95adc1f06dc0693fb180604b1cbf
rodolphoerick <rodolphoerick@info.ufrn.br>
2019-10-01 16:20:23 -0300

    #Merge-sort para inteiros.

---
+++ src/main/Main.java
@@ -0,0 +1,24 @@
+package main;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] array = {12, 11, 13, 5, 6, 7, 1, 0, 99, 101};
+
+        System.out.println("Given array is:");
+        printArray(array);
+
+        MergeSort mergeSort = new MergeSort();
+        mergeSort.sort(array, 0, array.length - 1);
+
+        System.out.println("\nSorted array is:");
+        printArray(array);
+    }
+
+    private static void printArray(int[] array) {
+        for (int i = 0; i < array.length; ++i)
+            System.out.print(array[i] + " ");
+        System.out.println();
+    }
+}
---
+++ src/main/MergeSort.java
@@ -0,0 +1,56 @@
+package main;
+
+public class MergeSort {
+
+    public void sort(int[] array, int indiceInicio, int indiceFim) {
+        if (indiceInicio < indiceFim) {
+            int indiceMedio = (indiceInicio + indiceFim) / 2;
+
+            sort(array, indiceInicio, indiceMedio);
+            sort(array, indiceMedio + 1, indiceFim);
+
+            merge(array, indiceInicio, indiceMedio, indiceFim);
+        }
+    }
+
+    private void merge(int[] array, int indiceInicio, int indiceMedio, int indiceFim) {
+
+        int tamanhoArrayEsquerda = indiceMedio - indiceInicio + 1;
+        int tamanhoArrayDireita = indiceFim - indiceMedio;
+
+        int[] arrayTempEsquerda = new int[tamanhoArrayEsquerda];
+        int[] arrayTempDireita = new int[tamanhoArrayDireita];
+
+        for (int i = 0; i < tamanhoArrayEsquerda; i++) {
+            arrayTempEsquerda[i] = array[indiceInicio + i];
+        }
+        for (int j = 0; j < tamanhoArrayDireita; j++) {
+            arrayTempDireita[j] = array[indiceMedio + 1 + j];
+        }
+
+        int i = 0, j = 0;
+        int k = indiceInicio;
+        while (i < tamanhoArrayEsquerda && j < tamanhoArrayDireita) {
+            if (arrayTempEsquerda[i] <= arrayTempDireita[j]) {
+                array[k] = arrayTempEsquerda[i];
+                i++;
+            } else {
+                array[k] = arrayTempDireita[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < tamanhoArrayEsquerda) {
+            array[k] = arrayTempEsquerda[i];
+            i++;
+            k++;
+        }
+
+        while (j < tamanhoArrayDireita) {
+            array[k] = arrayTempDireita[j];
+            j++;
+            k++;
+        }
+    }
+}
